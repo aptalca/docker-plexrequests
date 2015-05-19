11b0a9d4058967c55655304523d771e442a67d2e
aptalca <thedjinn@yahoo.com>
2015-05-19 17:09:49 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -4,13 +4,18 @@ cd /config
 
 if [ ! -d "/config/plexrequests-meteor" ]; then
   git clone https://github.com/lokenx/plexrequests-meteor.git
+  cd plexrequests-meteor
 else
   cd plexrequests-meteor
   git pull
 fi
 
+if [ "$EDGE" = "1" ]; then
+  echo "Switching to dev branch"
+  git checkout dev
+fi
+
 chown -R nobody:users /config
 chmod -R g+rw /config
 
-cd /config/plexrequests-meteor
 su -c "meteor &" -s /bin/sh nobody
