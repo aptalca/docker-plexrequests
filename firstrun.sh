24fd8703360d7e73efd39bf4c0f1d7e6ecd356b8
aptalca <thedjinn@yahoo.com>
2015-05-18 21:04:12 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 cd /config
-git clone https://github.com/lokenx/plexrequests-meteor.git
-cd plexrequests-meteor
+
+if [ ! -d "/config/plexrequests-meteor" ]; then
+  git clone https://github.com/lokenx/plexrequests-meteor.git
+else
+  cd plexrequests-meteor
+  git pull
+fi
+
 chown -R nobody:users /config
 chmod -R g+rw /config
 
+cd /config/plexrequests-meteor
 su -c "meteor &" -s /bin/sh nobody
